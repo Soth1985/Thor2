fd2d50bd01504cc82c212b937ca31369a404b06f
Victor Rykov <soth1985@yahoo.com>
2016-05-17 01:22:07 +0700

    add

---
+++ lib/Core/include/CoreExport.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Thor/Framework/Common.h>
+
+#ifdef THOR_MS_WIN
+	#ifdef THOR_FRAMEWORK_EXPORT
+		#define THOR_FRAMEWORK_DLL __declspec(dllexport)
+	#elif defined THOR_FRAMEWORK_IMPORT
+		#define THOR_FRAMEWORK_DLL __declspec(dllimport)
+	#else
+		#define THOR_FRAMEWORK_DLL 
+	#endif
+#else 
+		#define THOR_FRAMEWORK_DLL
+#endif
+
+namespace Thor
+{
+	struct eSingletonsLongevity
+	{
+		enum Val
+		{
+			TypeInfo,
+			RttiManager
+		};
+	};
+}
